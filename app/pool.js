f316a01359af912952a4393f830b2887e5c183e2
Mr-Wiredancer <lijiahao90@gmail.com>
2015-04-11 16:21:33 +0800

    added mysql on heroku

--- app/pool.js
+++ app/pool.js
@@ -1,9 +1,11 @@
 var mysql = require('mysql');
-var pool = mysql.createPool({
-  database: 'wifimap',
-  host: 'localhost',
-  user: 'root'
-});
+var pool = mysql.createPool(
+  process.env.DATABASE_URL || 
+  {
+    database: 'wifimap',
+    host: 'localhost',
+    user: 'root'
+  });
 
 module.exports = pool;
 
