2107f4ff71a8097c8aa39db8c44a02bfd48a1647
Goldenstone <hxljustdoit@gmail.com>
2015-04-13 16:03:31 +0800

    change seed.sql

--- app/seed.sql
+++ app/seed.sql
@@ -4,10 +4,23 @@ use wifimap;
 create table wifis
 (
   id int unsigned NOT NULL auto_increment,
+  adopter varchar(255) NOT NULL,
   name varchar(255) NOT NULL,
-  owner varchar(255) NOT NULL,
   latitude decimal(10, 7) NOT NULL,
   longitude decimal(10, 7) NOT NULL,
+  business varchar(255)
+
+  primary key (id)
+)
+
+create table applications
+(
+  id int unsigned NOT NULL auto_increment,
+  email varchar(255) NOT NULL,
+  adopter varchar(255) NOT NULL,
+  wifi varchar(255) NOT NULL,
+  location varchar(255) NOT NULL,
+  business varchar(255),
 
   primary key (id)
 )
