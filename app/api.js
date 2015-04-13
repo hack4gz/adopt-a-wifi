03e69eaa1b381ccdf89f5bcd6956d6e092d66dfa
Goldenstone <hxljustdoit@gmail.com>
2015-04-13 15:39:40 +0800

    add auth for api/applications and /applications

--- app/api.js
+++ app/api.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var wifisHandler = require('./api/wifis');
 var applicationsHandler = require('./api/applications');
+var auth = require('./auth');
 
 var app = express();
 app.use('/wifis', wifisHandler);
-app.use('/applications', applicationsHandler);
+app.use('/applications', auth, applicationsHandler);
 
 module.exports = app;
--- app/routes.js
+++ app/routes.js
@@ -1,26 +1,9 @@
 var index = require('./routes/index');
 var application = require('./routes/applications');
 var apiServer = require('./api');
-var basicAuth = require('basic-auth');
+var auth = require('./auth');
 
 module.exports = function(app) {
-  var auth = function (req, res, next) {
-    function unauthorized(res) {
-      res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
-      return res.send(401);
-    }
-    var user = basicAuth(req);
-
-    if (!user || !user.name || !user.pass) {
-      return unauthorized(res);
-    }
-
-    if (user.name === 'wifimap' && user.pass === '1234567') {
-      return next();
-    } else {
-      return unauthorized(res);
-    }
-  };
   app.use('/', index);
   app.use('/applications', auth, application);
   app.use('/api', apiServer);
